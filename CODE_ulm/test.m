b8837dec61cf981b892c6b0500adb49bc0f0f95a
felixtraub <30345758+felixtraub@users.noreply.github.com>
2017-09-18 12:42:57 +0200

    Delete test.m

--- CODE_ulm/test.m
+++
@@ -1,6 +0,0 @@
-%(a short test)
-p1 = serial('/dev/cu.usbserial-ftEGHNL6','BaudRate',57600);                %the port name depend on the platform
-fopen(p1);
-%is NV blocking command?
-fprintf(p1,'LR%i\n SP%i\n AC%i\n NV%i\n M\n AC%i\n NV%i\n M\n', [415000 2000 10 100 3000 2000]);
-fclose(p1);
